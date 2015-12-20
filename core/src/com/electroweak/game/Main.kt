3efc0fe7a188b1126727a4ed1ac502df46d44f95
ElectroWeak1 <electroweak1@gmail.com>
2015-12-20 15:20:27 +0100

    Set up Kotlin

--- core/src/com/electroweak/game/Main.kt
+++ core/src/com/electroweak/game/Main.kt
@@ -1,28 +1,12 @@
 package com.electroweak.game
 
-import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
-class Main : ApplicationAdapter() {
+class Main : Game() {
 
-    internal lateinit var batch: SpriteBatch
-    internal lateinit var img: Texture
+    override fun create() {}
 
-    @Override
-    override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
-
-    @Override
-    override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
 }
